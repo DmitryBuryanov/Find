106505cabf72527f4dc4b71069ffd526c46f8fb5
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2020-04-01 19:00:18 +0300

    Исправлены замечания

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -14,32 +14,34 @@ public class Tests {
 
     @Test
     public void test1() throws Exception {
-        Path path2 = Paths.get(findfile.launch(new String[]{"-r", "-d", "TestDirectory", "TestFile1.txt"}));
+        Path path2 = Paths.get(findfile.launch(new String[]{"-r", "-d", "testDirectory", "TestFile1.txt"}));
 
-        assertEquals("TestDirectory\\TestFile1.txt", path1.relativize(path2).toString());
+        assertEquals("testDirectory\\TestFile1.txt", path1.relativize(path2).toString());
     }
 
     @Test
     public void test2() throws Exception {
-        Path path2 = Paths.get(findfile.launch(new String[]{"-r", "-d", "TestDirectory\\TestDirectory2", "TestFile4.txt"}));
+        Path path2 = Paths.get(findfile.launch(new String[]{"-r", "-d",
+                "testDirectory\\testDirectory2", "TestFile4.txt"}));
 
-        assertEquals("TestDirectory\\TestDirectory2\\TestDirectory3\\TestFile4.txt", path1.relativize(path2).toString());
+        assertEquals("testDirectory\\testDirectory2\\testDirectory3\\TestFile4.txt",
+                path1.relativize(path2).toString());
     }
 
     @Test
     public void test3() throws Exception {
-        assertEquals("File is not found", findfile.launch(new String[]{"-d", "TestDirectory", "TestFile4.txt"}));
+        assertEquals("File is not found", findfile.launch(new String[]{"-d", "testDirectory", "TestFile4.txt"}));
     }
 
     @Test
     public void test4() throws Exception {
         Path path2 = Paths.get(findfile.launch(new String[]{"-r", "TestFile1.txt"}));
-        assertEquals("TestDirectory\\TestFile1.txt", path1.relativize(path2).toString());
+        assertEquals("testDirectory\\TestFile1.txt", path1.relativize(path2).toString());
     }
 
     @Test
     public void test5() throws Exception {
-        assertEquals("File is not found", findfile.launch(new String[]{"-r", "-d", "TestDirectory", "TestFile5.txt"}));
+        assertEquals("File is not found", findfile.launch(new String[]{"-r", "-d", "testDirectory", "TestFile5.txt"}));
     }
 
 
